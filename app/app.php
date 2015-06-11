4cef6539d83a9222d1979be643e8a8cda148db72
zeyu203 <zeyu203@qq.com>
2015-06-11 15:21:48 +0800

    删除调试代码

--- app/app.php
+++ app/app.php
@@ -3,6 +3,4 @@ ini_set('date.timezone','Asia/Shanghai');
 require_once(__DIR__.'/Dispatcher.php');
 
 Dispatcher::getInstance()->dispatch();
-exit;
-Dispatcher::getInstance('debug')->dispatch();
 ?>
