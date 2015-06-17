bdc8f4f5bf8cd9d514123eadcf7e6f31c62bc47f
zeyu203 <zeyu203@qq.com>
2015-06-17 10:05:08 +0800

    兼容测试

--- web/app.php
+++ web/app.php
@@ -2,5 +2,9 @@
 ini_set('date.timezone','Asia/Shanghai');
 require_once(__DIR__.'/../app/Dispatcher.php');
 
-Dispatcher::getInstance()->dispatch();
+if (strpos(__DIR__, 'example_techlog') !== false) {
+	#Dispatcher::getInstance('debug')->dispatch();
+} else {
+	Dispatcher::getInstance()->dispatch();
+}
 ?>
