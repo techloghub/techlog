1cd482ab52062d2a56acd15482694f401266d9d1
zeyu203 <zeyu203@qq.com>
2015-06-25 11:36:56 +0800

    目录切换
    Conflicts:
    	web/app.php

--- web/app.php
+++ web/app.php
@@ -2,9 +2,13 @@
 ini_set('date.timezone','Asia/Shanghai');
 require_once(__DIR__.'/../app/Dispatcher.php');
 
+<<<<<<< HEAD
 if (strpos(__DIR__, 'example_techlog') !== false) {
 	Dispatcher::getInstance('debug')->dispatch();
 } else {
 	Dispatcher::getInstance()->dispatch();
 }
+=======
+Dispatcher::getInstance()->dispatch();
+>>>>>>> d627a3a... 目录切换
 ?>
