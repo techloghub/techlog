0f66e5d9fd0f3419deb4413cd01d0958703bcc48
zeyu203 <zeyu203@qq.com>
2015-06-25 11:37:17 +0800

    增加访问信息页面
    Conflicts:
    	web/app.php

--- web/app.php
+++ web/app.php
@@ -4,6 +4,7 @@ require_once(__DIR__.'/../app/Dispatcher.php');
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 if (strpos(__DIR__, 'example_techlog') !== false) {
 	Dispatcher::getInstance('debug')->dispatch();
 } else {
@@ -15,4 +16,7 @@ Dispatcher::getInstance()->dispatch();
 =======
 #Dispatcher::getInstance('debug')->dispatch();
 >>>>>>> 1976c14... 目录调整，提高安全性
+=======
+Dispatcher::getInstance()->dispatch();
+>>>>>>> 105d5b0... 增加访问信息页面
 ?>
