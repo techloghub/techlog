080a589ba719d1ac96ad53afbe63f3664759d5cb
zeyu203 <zeyu203@qq.com>
2015-07-25 20:19:05 +0800

    EarningsController mysql->pdo

--- controller/EarningsController.php
+++ controller/EarningsController.php
@@ -13,13 +13,19 @@ class EarningsController extends Controller
 		$det_end = empty($_GET['end']) ? '2013-09' : $_GET['end'];
 		if ($det_beg > $det_end)
 			list($det_beg, $det_end) = array($det_end, $det_beg);
-		$drw_beg = empty($_GET['drw_beg']) ? date('Y-m', time()) : $_GET['drw_beg'];
+		$drw_beg = empty($_GET['drw_beg']) ?
+			date('Y-m', time()) : $_GET['drw_beg'];
 		$drw_end = empty($_GET['drw_end']) ? '2013-09' : $_GET['drw_end'];
 		if ($drw_beg > $drw_end)
 			list($drw_beg, $drw_end) = array($drw_end, $drw_beg);
 
 		$query = 'select * from earnings order by month desc limit 24';
-		$earnings = MySqlOpt::select_query($query);
+		$earnings = Repository::findFromEarnings(
+			array(
+				'order' => array('month' => 'desc'),
+				'range' => array(0, 24)
+			)
+		);
 		$earn_infos = array();
 		$month = array();
 		$income = array();
@@ -31,31 +37,30 @@ class EarningsController extends Controller
 		{
 			$infos = array();
 
-			if ($earning['month'] >= $det_beg
-				and $earning['month'] <= $det_end
+			if ($earning->get_month() >= $det_beg
+				and $earning->get_month() <= $det_end
 				and $index < 12
 			)
 			{
-				$infos['idx_href'] = '/article/list/'.$earning['article_id'];
+				$infos['idx_href'] = '/article/list/'.$earning->get_article_id();
 
-				$image_select_query =
-					'select path from images where image_id='.$earning['image_id'];
-				$image_ret = MySqlOpt::select_query($image_select_query);
+				$path = Repository::findPathFromImages(
+					array('eq' => array('image_id' => $earning->get_image_id()))
+				);
 
-				$path = $image_ret[0]['path'];
 				$infos['image_path'] = $path;
-				$infos['title'] = $earning['month'];
-				$infos['descs'] =
-					'结余:&nbsp;&nbsp;'.($earning['income']-$earning['expend']);
+				$infos['title'] = $earning->get_month();
+				$infos['descs'] = '结余:&nbsp;&nbsp;'
+					.($earning->get_income() - $earning->get_expend());
 				$earn_infos[] = $infos;
 				$index++;
 			}
 
-			if ($earning['month'] >= $drw_beg and $earning['month'] <= $drw_end)
+			if ($earning->get_month() >= $drw_beg and $earning->get_month() <= $drw_end)
 			{
-				$month[] = $earning['month'];
-				$income[] = $earning['income'];
-				$expend[] = $earning['expend'];
+				$month[] = $earning->get_month();
+				$income[] = $earning->get_income();
+				$expend[] = $earning->get_expend();
 			}
 		}
 		$average = round((array_sum($income)-array_sum($expend))/count($month), 2);
