92fca1a793b9e3259d1cdee2803fee2ebc3ffa77
zeyu203 <zeyu203@qq.com>
2015-07-26 18:10:21 +0800

    重构代码，结构清晰

--- app/register.php
+++ app/register.php
@@ -1,12 +1,4 @@
 <?php
-define('APP_PATH', __DIR__);
-define('WEB_PATH', __DIR__.'/../web');
-define('LIB_PATH', __DIR__.'/../library');
-define('VIEW_PATH', __DIR__.'/../views');
-define('DRAFT_PATH', __DIR__.'/../draft');
-define('MODEL_PATH', __DIR__.'/../model');
-define('RESOURCE_PATH', __DIR__.'/../resource');
-define('CONTROLLER_PATH', __DIR__.'/../controller');
 require_once(APP_PATH.'/file_register.php');
 
 $controller_list = array(
