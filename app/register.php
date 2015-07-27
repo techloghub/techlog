86fcfb1eb9fde4bcbed3f3769b0007520c519f9f
zeyu203 <zeyu203@qq.com>
2015-07-27 10:20:30 +0800

    LogOpt

--- app/register.php
+++ app/register.php
@@ -9,6 +9,7 @@ define('RESOURCE_PATH', __DIR__.'/../resource');
 define('CONTROLLER_PATH', __DIR__.'/../controller');
 
 require_once(LIB_PATH.'/StringOpt.php');
+require_once(LIB_PATH.'/LogOpt.php');
 require_once(LIB_PATH.'/Controller.php');
 require_once(LIB_PATH.'/Repository.php');
 require_once(APP_PATH.'/Dispatcher.php');
