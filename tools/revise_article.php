fe7855ce38a236eb85ae553be8982e0808c11ca6
zeyu203 <zeyu203@qq.com>
2015-07-28 15:09:32 +0800

    tools/revise_article.php mysql -> pdo

--- tools/revise_article.php
+++ tools/revise_article.php
@@ -1,6 +1,5 @@
 <?php
 require_once (__DIR__.'/../app/register.php');
-require_once (LIB_PATH.'/TechlogTools.php');
 
 LogOpt::init('revise_article', true);
 
@@ -10,25 +9,18 @@ if (!isset ($options['i']) || !is_numeric($options['i']))
 	echo 'usage: php revise_article.php -i article_id'.PHP_EOL;
 	return;
 }
-$query = 'select draft,title from article where article_id='.$options['i'];
-$draft = MySqlOpt::select_query($query);
-if ($draft == null)
+$draft = Repository::findDraftFromArticle(
+	array('eq' => array('article_id' => $options['i'])));
+$title = Repository::findTitleFromArticle(
+	array('eq' => array('article_id' => $options['i'])));
+if ($draft == false || $title == false)
 {
 	LogOpt::set ('exception', 'cannot find the article',
-		'article_id', $options['i'],
-		MySqlOpt::errno(), MySqlOpt::error());
-
+		'article_id', $options['i']);
 	return;
 }
-$title = $draft[0]['title'];
-$draft = $draft[0]['draft'];
-
 $filename = DRAFT_PATH.'/draft'.$options['i'].'.tpl';
-
 file_put_contents($filename, $draft);
-
 LogOpt::set('info', '已将文件加载至'.$filename,
-	'article_id', $options['i'],
-	'title', $title
-);
+	'article_id', $options['i'], 'title', $title);
 ?>
