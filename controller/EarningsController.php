b09949332ba957d8a6ad8d4d5e388313e295c268
zeyu203 <zeyu203@qq.com>
2015-09-04 16:33:14 +0800

    重构财报页面

--- controller/EarningsController.php
+++ controller/EarningsController.php
@@ -1,6 +1,18 @@
 <?php
 class EarningsController extends Controller
 {
+	private $colors = array(
+		array('rgba(0, 255, 255, 0.7)', 'rgba(220, 220, 220, 0.3)'),
+		array('rgba(17, 177, 255, 0.7)', 'rgba(220, 220, 220, 0.3)'),
+		array('rgba(128, 128, 255, 0.7)', 'rgba(220, 220, 220, 0.3)'),
+		array('rgba(255, 128, 192, 0.7)', 'rgba(220, 220, 220, 0.3)'),
+		array('rgba(104, 180, 255, 0.7)', 'rgba(220, 220, 220, 0.3)'),
+		array('rgba(255, 255, 128, 0.7)', 'rgba(220, 220, 220, 0.3)'),
+		array('rgba(128, 255, 128, 0.7)', 'rgba(220, 220, 220, 0.3)'),
+	);
+	private $income;
+	private $expend;
+
 	public function listAction()
 	{
 		if (!$this->is_root)
@@ -9,17 +21,146 @@ class EarningsController extends Controller
 			return;
 		}
 
+		$avg = $this->getAvg(false);
+		list($labels, $expends, $incomes) = $this->getInOutDatas(24, false);
+		list($inCategories, $outCategories) = $this->getCategories('', '', false);
+
+		$params = array(
+			'incomes' => json_encode($incomes),
+			'expends' => json_encode($expends),
+			'labels' => json_encode($labels),
+			'inCategories' => json_encode($inCategories),
+			'outCategories' => json_encode($outCategories),
+			'avg' => $avg,
+			'income' => $this->income,
+			'expend' => $this->expend,
+		);
+
+		$this->display(__METHOD__, $params);
+	}
+
+	private function getCategories($begMonth, $endMonth, $useHouseFund)
+	{
+		$query_params = array();
+		if (!$useHouseFund)
+		{
+			$query_params['query']['bool']['must_not'][] = array('term' =>
+				array('fromAcc' => '0b45af0bd5e741dbbe8e796f13943736'));
+		}
+		$query_params['query']['bool']['must'][] =
+			array('term' => array('recType' => 2));
+		if (!empty($begMonth))
+		{
+			$query_params['query']['bool']['must'][] = array('range' =>
+				array('date' => array('gte' => $begMonth.'-01 00:00:00')));
+		}
+		if (!empty($endMonth))
+		{
+			$enddate = date('Y-m-t 23:59:59', strtotime($endMonth));
+			$query_params['query']['bool']['must'][] = array('range' =>
+				array('date' => array('lte' => $enddate)));
+		}
+		$query_params['size'] = 0;
+		$query_params['aggs']['uniq'] = array(
+			'terms' => array('field' => 'category', 'size' => 0),
+			'aggs' => array('totalfee' => array('sum' => array('field' => 'money')))
+		);
+		$ret = ESRepository::getWacaiLedgersList($query_params);
+		if ($ret === false || !isset($ret['aggregations']['uniq']['buckets']))
+		{
+			header("Location: /index/notfound");
+			exit;
+		}
+		$inCategories = array();
+		$outCategories = array();
+		$i = 0;
+		foreach ($ret['aggregations']['uniq']['buckets'] as $infos)
+		{
+			$inCategories[] = array(
+				'label' => $infos['key'].' ('
+					.round($infos['totalfee']['value']*100/$this->income, 2).'%)',
+				'value' => round($infos['totalfee']['value'], 2),
+				'color' => $this->colors[$i%count($this->colors)][0],
+				'highlight' => $this->colors[$i%count($this->colors)][1],
+			);
+			$i++;
+		}
+
+		$query_params['query']['bool']['must'][0]['term']['recType'] = 1;
+		$ret = ESRepository::getWacaiLedgersList($query_params);
+		if ($ret === false || !isset($ret['aggregations']['uniq']['buckets']))
+		{
+			header("Location: /index/notfound");
+			exit;
+		}
+		$i = 0;
+		foreach ($ret['aggregations']['uniq']['buckets'] as $infos)
+		{
+			$outCategories[] = array(
+				'label' => $infos['key'].' ('
+					.round($infos['totalfee']['value']*100/$this->expend, 2).'%)',
+				'value' => round($infos['totalfee']['value'], 2),
+				'color' => $this->colors[$i%count($this->colors)][0],
+				'highlight' => $this->colors[$i%count($this->colors)][1],
+			);
+			$i++;
+		}
+
+		return array($inCategories, $outCategories);
+	}
+
+	private function getAvg($useHouseFund)
+	{
+		$time = time();
+		$query_params = array();
+		if (!$useHouseFund)
+		{
+			$query_params['query']['bool']['must_not'][] = array('term' =>
+				array('fromAcc' => '0b45af0bd5e741dbbe8e796f13943736'));
+		}
+		$query_params['query']['bool']['must'][] =
+			array('term' => array('recType' => 2));
+		$query_params['aggs']['totalfee'] =
+			array('sum' => array('field' => 'money'));
+		$query_params['size'] = 0;
+		$ret = ESRepository::getWacaiLedgersList($query_params);
+		if ($ret === false || !isset($ret['aggregations']['totalfee']['value']))
+		{
+			header("Location: /index/notfound");
+			exit;
+		}
+		$this->income = round($ret['aggregations']['totalfee']['value'], 2);
+		$query_params['query']['bool']['must'][0]['term']['recType'] = 1;
+		$ret = ESRepository::getWacaiLedgersList($query_params);
+		if ($ret === false || !isset($ret['aggregations']['totalfee']['value']))
+		{
+			header("Location: /index/notfound");
+			exit;
+		}
+		$this->expend = round($ret['aggregations']['totalfee']['value']);
+		$avg = $this->income - $this->expend;
+		$avg = round($avg/(($time - 1377964800)/(3600*24*30)), 2);
+		return $avg;
+	}
+
+	private function getInOutDatas($monthCount, $useHouseFund)
+	{
 		$time = time();
 		$labels = array();
 		$expends = array();
 		$incomes = array();
-		for ($i=24; $i>=1; $i--)
+		for ($i=$monthCount; $i>=1; $i--)
 		{
 			$query_params = array();
 			$beg_month = date('Y-m-01 00:00:00', $time - $i*3600*24*30);
 			$end_month = date('Y-m-t 23:59:59', $time - $i*3600*24*30);
 			$query_params['query']['bool']['must'][] =
 				array('term' => array('recType' => 1));
+			if (!$useHouseFund)
+			{
+				$query_params['query']['bool']['must_not'][] = array('term' =>
+					array('fromAcc' => '0b45af0bd5e741dbbe8e796f13943736'));
+			}
 			$query_params['query']['bool']['must'][] =
 				array('range' => array('date' => array( 'gte' => $beg_month,
 							'lte' => $end_month)));
@@ -27,31 +168,28 @@ class EarningsController extends Controller
 				array('sum' => array('field' => 'money'));
 			$query_params['size'] = 0;
 			$ret = ESRepository::getWacaiLedgersList($query_params);
-			if ($ret === false || !isset($ret['aggregations']['totalfee']['value']))
+			if ($ret === false
+				|| !isset($ret['aggregations']['totalfee']['value']))
 			{
+				exit;
 				header("Location: /index/notfound");
-				return;
+				exit;
 			}
 			$expends[] = round($ret['aggregations']['totalfee']['value'], 2);
 
 			$query_params['query']['bool']['must'][0]['term']['recType'] = 2;
 			$ret = ESRepository::getWacaiLedgersList($query_params);
-			if ($ret === false || !isset($ret['aggregations']['totalfee']['value']))
+			if ($ret === false
+				|| !isset($ret['aggregations']['totalfee']['value']))
 			{
 				header("Location: /index/notfound");
-				return;
+				exit;
 			}
 			$incomes[] = round($ret['aggregations']['totalfee']['value'], 2);
 
 			$labels[] = date('Y-m', $time - $i*3600*24*30);
 		}
-		$params = array(
-			'incomes' => json_encode($incomes),
-			'expends' => json_encode($expends),
-			'labels' => json_encode($labels),
-		);
-
-		$this->display(__METHOD__, $params);
+		return array($labels, $expends, $incomes);
 	}
 }
 ?>
--- views/earnings/list.php
+++ views/earnings/list.php
@@ -4,10 +4,23 @@
 	<div class="row">
 		<canvas id="out_in_lines" width="980px" height="300px"></canvas>
 		<div class='span12'><br /></div>
+		<div class='span2'>
+		</div>
+		<div class='span2'>
+		</div>
+		<div class='span2'>
+			<input type="text" class="form-control" readonly="readonly" value="支出：<?php echo $params['expend'] ?>"/>
+		</div>
+		<div class='span2'>
+			<input type="text" class="form-control" readonly="readonly" value="收入：<?php echo $params['income'] ?>"/>
+		</div>
+		<div class='span2'>
+			<input type="text" class="form-control" readonly="readonly" value="月均结余：<?php echo $params['avg'] ?>"/>
+		</div>
 		<div class='span12'><br /><br /></div>
-		<canvas id="category_doughnut" width="300px" height="300px" class="span4"></canvas>
-		<div class="span1"></div>
-		<canvas id="category_doughnut" width="300px" height="300px" class="span4"></canvas>
+		<canvas id="out_category_doughnut" width="300px" height="300px" class="span4"></canvas>
+		<div class="span2"></div>
+		<canvas id="in_category_doughnut" width="300px" height="300px" class="span4"></canvas>
 		<div class='span12'><br /><br /></div>
 	</div>
 </div>
@@ -21,7 +34,7 @@ var out_in_datas =
 	[
 		{
 			label: "income",
-			fillColor : "rgba(220,220,220,0.3)",
+			fillColor : "rgba(220,220,220,0.5)",
 			strokeColor : "rgba(220,220,220,1)",
 			pointColor : "rgba(220,220,220,1)",
 			pointStrokeColor : "#fff",
@@ -29,7 +42,7 @@ var out_in_datas =
 		},
 		{
 			label : "expend",
-			fillColor : "rgba(68,114,169,0.3)",
+			fillColor : "rgba(68,114,169,0.5)",
 			strokeColor : "rgba(151,187,205,1)",
 			pointColor : "rgba(151,187,205,1)",
 			pointStrokeColor : "#fff",
@@ -40,5 +53,13 @@ var out_in_datas =
 var ctx = document.getElementById("out_in_lines").getContext("2d");
 var myNewChart = new Chart(ctx).PolarArea(out_in_datas);
 new Chart(ctx).Line(out_in_datas, canvas_options);
+
+doughnut_options['percentageInnerCutout'] = 30;
+var in_categories_data = <?php echo $params['inCategories'] ?>;
+var out_categories_data = <?php echo $params['outCategories'] ?>;
+var ctx = document.getElementById("in_category_doughnut").getContext("2d");
+var inDoughnutChart = new Chart(ctx).Doughnut(in_categories_data, doughnut_options);
+var ctx = document.getElementById("out_category_doughnut").getContext("2d");
+var outDoughnutChart = new Chart(ctx).Doughnut(out_categories_data, doughnut_options);
 </script>
 <?php require(VIEW_PATH.'/base/footer.php'); ?>
