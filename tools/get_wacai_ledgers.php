309ca47ba65596a3c24a9732b6a99c4bc4071d1d
zeyu203 <zeyu203@qq.com>
2016-08-01 08:46:18 +0800

    更新 get_wacai_ledgers.php

--- tools/get_wacai_ledgers.php
+++ tools/get_wacai_ledgers.php
@@ -33,136 +33,120 @@ for ($i=1; $i<=$pageCount; $i++)
 	$ledgers = $infos['ledgers'];
 	foreach ($ledgers as $infos)
 	{
-		if (!isset($infos['date']) || !isset($infos['recType']))
+		if (empty($infos['id']) || !isset($infos['date']) || !isset($infos['recType']))
 		{
 			echo 'INFOS ERROR : '.json_encode($infos).PHP_EOL;
 			continue;
 		}
-		$es_params = array();
-		$es_params['date']			=
-			(isset($infos['date']) ? $infos['date'] : '');
-		$es_params['recType']		=
-			(isset($infos['recType']) ? $infos['recType'] : '');
-		$es_params['tag'] 			=
-			(isset($infos['tag']) ? $infos['tag'] : '');
-		$es_params['comment'] 		=
-			(isset($infos['comment']) ? $infos['comment'] : '');
-		$es_params['inserttime']	= date('Y-m-d H:i:s', time());
-		if ($es_params['recType'] == 3)
+		$model = new LedgersModel(
+			array(
+				'esid' => isset($infos['id']) ? $infos['id'] : '',
+				'date' => isset($infos['date']) ? $infos['date'] : '',
+				'recType' => isset($infos['recType']) ? $infos['recType'] : '',
+				'tag' => isset($infos['tag']) ? $infos['tag'] : '',
+				'comment' => isset($infos['comment']) ? $infos['comment'] : '',
+				'inserttime' => 'now()'
+			)
+		);
+		if ($model->get_recType() == 3)
 		{
-			$es_params['type'] = -1;
-			$es_params['fromAcc'] = $infos['srcAcc'];
-			$es_params['toAcc'] = $infos['tgtAcc'];
-			$es_params['money'] = $infos['transin'];
+			$model->set_type(-1);
+			$model->set_fromAcc($infos['srcAcc']);
+			$model->set_toAcc($infos['tgtAcc']);
+			$model->set_money($infos['transin']);
 			$infos['mflag'] = $infos['srcMflag'];
 		}
-		else if ($es_params['recType'] == 4)
+		else if ($model->get_recType() == 4)
 		{
-			$es_params['type'] = $infos['type'];
-			$es_params['fromAcc'] = $infos['acc'];
-			$es_params['toAcc'] = $infos['srcAcc'];
-			$es_params['money'] = $infos['money'];
+			$model->set_type($infos['type']);
+			$model->set_fromAcc($infos['acc']);
+			$model->set_toAcc($infos['srcAcc']);
+			$model->set_money($infos['money']);
 		}
-		else if ($es_params['recType'] == 5)
+		else if ($model->get_recType() == 5)
 		{
-			$es_params['type'] = $infos['type'];
-			$es_params['fromAcc'] = $infos['srcAcc'];
-			$es_params['toAcc'] = $infos['acc'];
-			$es_params['money'] = $infos['money'];
+			$model->set_type($infos['type']);
+			$model->set_fromAcc($infos['srcAcc']);
+			$model->set_toAcc($infos['acc']);
+			$model->set_money($infos['money']);
 		}
 		else
 		{
-			$es_params['type'] = $infos['type'];
-			$es_params['fromAcc'] = $infos['acc'];
-			$es_params['toAcc'] = '';
-			$es_params['money'] = $infos['money'];
+			$model->set_type($infos['type']);
+			$model->set_fromAcc($infos['acc']);
+			$model->set_toAcc('');
+			$model->set_money($infos['money']);
 		}
 		switch ($infos['mflag'])
 		{
 		case '￥':
-			$es_params['currency'] = '人民币';
+			$model->set_currency('人民币');
 			break;
 		case '◎':
-			$es_params['currency'] = '虚拟币';
+			$model->set_currency('虚拟币');
 			break;
 		case '$':
-			$es_params['currency'] = '美元';
+			$model->set_currency('美元');
 			break;
 		default:
-			$es_params['currency'] = '';
+			$model->set_currency('');
 			break;
 		}
 		$category = explode('-', $infos['typeTitle']);
-		$es_params['category'] = $category[0];
-		$es_params['subcategory'] = (isset($category[1]) ? $category[1] : '');
+		$model->set_category($category[0]);
+		$model->set_subcategory((isset($category[1]) ? $category[1] : ''));
+		$id = Repository::persist($model);
 
-		$es_url = 'http://localhost:9200/wacai/ledgers/'.$infos['id'].'/_create';
-		$ret = HttpCurl::post($es_url, json_encode($es_params));
-		if ($ret['code'] == 409)
-		{
-			echo 'WARNING: DUPLICATE RECORD'."\t".$infos['id']."\t"
-				.json_encode($es_params).PHP_EOL;
-			continue;
-		}
-		else if ($ret['body'] == false
-			|| !in_array($ret['code'], array(200, 201)))
-		{
-			var_dump($ret);
-			exit;
-		}
 		echo 'INFO: BACKUP'."\t".$infos['id']."\t"
-			.json_encode($es_params).PHP_EOL;
+			.json_encode($infos).PHP_EOL;
 
-		$acc_url = 'http://localhost:9200/wacai/account/'.$es_params['fromAcc'];
-		$ret = HttpCurl::get($acc_url);
-		$ret = $ret['body'];
-		$acc_infos = json_decode($ret, true);
-		if ($acc_infos != false)
+		$account = Repository::findOneFromAccount(
+			array('eq' => array('esid' => $model->get_fromAcc()))
+		);
+		if ($account != false)
 		{
-			$acc_infos = $acc_infos['_source'];
-			if ($es_params['recType'] == 2)
-				$acc_infos['money'] += $es_params['money'];
+			if ($model->get_recType() == 2)
+				$account->set_money($model->get_money() + $account->get_money());
 			else
-				$acc_infos['money'] -= $es_params['money'];
-			$acc_infos['orderNo']++;
-			$acc_infos['updatetime'] = date('Y-m-d H:i:s', time());
-			$ret = HttpCurl::post($acc_url, json_encode($acc_infos));
+				$account->set_money($account->get_money() - $model->get_money());
+			$account->set_orderNo($account->get_orderNo() + 1);
+			$account->set_updatetime('now()');
+			$id = Repository::persist($account);
 		}
 
-		if (!isset($es_params['toAcc']))
+		if (empty($model->get_toAcc()))
 			continue;
-		$acc_url = 'http://localhost:9200/wacai/account/'.$es_params['toAcc'];
-		$ret = HttpCurl::get($acc_url);
-		$ret = $ret['body'];
-		$acc_infos = json_decode($ret, true);
-		if ($acc_infos == false)
+		$account = Repository::findOneFromAccount(
+			array('eq' => array('esid' => $model->get_toAcc()))
+		);
+		if ($account == false)
 			continue;
-		$acc_infos = $acc_infos['_source'];
-		$acc_infos['money'] += $es_params['money'];
-		$acc_infos['orderNo']++;
-		$acc_infos['updatetime'] = date('Y-m-d H:i:s', time());
-		HttpCurl::post($acc_url, json_encode($acc_infos));
+		$account->set_money($model->get_money() + $account->get_money());
+		$account->set_orderNo($account->get_orderNo() + 1);
+		$account->set_updatetime('now()');
+		$id = Repository::persist($account);
 	}
 	sleep(3);
 }
 
 function get_cookie()
 {
-	$url = 'https://www.wacai.com/user/user!login.action?cmd=null';
-	$config = file_get_contents(APP_PATH.'/config.json');
-	$config = json_decode($config, true);
-	$config = $config['wacai'];
-	$post_data = array(
-		'user.account' => $config['user'],
-		'user.pwd' => $config['pwd']
-	);
-	$ret = HttpCurl::post($url, $post_data);
-	if ($ret['body'] == false)
-	{
-		echo 'get_cookie ERROR: '.$ret['error'].PHP_EOL;
-		return false;
-	}
-	return (isset($ret['header']['set_cookie']) ?
-		$ret['header']['set_cookie'] : false);
+	#$url = 'https://www.wacai.com/user/user!login.action?cmd=null';
+	#$config = file_get_contents(APP_PATH.'/config.json');
+	#$config = json_decode($config, true);
+	#$config = $config['wacai'];
+	#$post_data = array(
+	#	'user.account' => $config['user'],
+	#	'user.pwd' => $config['pwd']
+	#);
+	#$ret = HttpCurl::post($url, $post_data);
+	#if ($ret['body'] == false)
+	#{
+	#	echo 'get_cookie ERROR: '.$ret['error'].PHP_EOL;
+	#	return false;
+	#}
+	#return (isset($ret['header']['set_cookie']) ?
+	#	$ret['header']['set_cookie'] : false);
+	return 'wctk=13f1b1021f9c43edaa4513c7fbc1889c';
 }
 ?>
