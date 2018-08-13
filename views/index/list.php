822e58bcd1939ec6e9646b0f91d94075af9b99a2
zeyu203 <zeyu203@qq.com>
2018-08-13 10:05:30 +0800

    增加任务管理

--- views/index/list.php
+++ views/index/list.php
@@ -15,9 +15,15 @@
 	<br />
 	<h1><p class="indexfont">龍潭齋</p></h1>
 	<br />
-	<p class="indexfont" style='font-family:"PT Serif","Georgia","Helvetica Neue",Arial,sans-serif' class="lead">這個世上只有兩樣東西永遠都不會衰老，一個是青春，一個是對知識的渴求</p><p style='font-family:"PT Serif","Georgia","Helvetica Neue",Arial,sans-serif;' class="indexfont"> ---- 熱愛生活，熱愛編碼，為了夢想奔跑的程序員</p>
+	<p class="indexfont" style='font-family:"PT Serif","Georgia","Helvetica Neue",Arial,sans-serif' class="lead">這個世上只有兩樣東西永遠都不會衰老，一個是青春，一個是對知識的渴求</p>
+	<p style='font-family:"PT Serif","Georgia","Helvetica Neue",Arial,sans-serif;' class="indexfont"> ---- 熱愛生活，熱愛編碼，為了夢想奔跑的程序員</p>
 	<br />
-	<p><a class="btn btn-lg btn-primary" href="/article/list/10182614" role="button" style='font-family:"Hiragino Sans GB", "Microsoft YaHei","WenQuanYi Micro Hei"'>博客簡介</a></p>
+	<p>
+		<a class="btn btn-lg btn-primary" href="/article/list/10182614" role="button" style='font-family:"Hiragino Sans GB", "Microsoft YaHei","WenQuanYi Micro Hei"'>博客簡介</a>
+		<?php if ($params['is_root']): ?>
+		<a class="btn btn-lg btn-primary" href="http://admin.techlog.cn/techlog_manager/tasklist/list" role="button" style='font-family:"Hiragino Sans GB", "Microsoft YaHei","WenQuanYi Micro Hei"'>任务管理</a>
+		<?php endif ?>
+	</p>
 </div>
 <br />
 <br />
