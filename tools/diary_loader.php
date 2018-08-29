5e31a69d8fcdfd8f2572f317dfa7ec8597a06303
zeyu203 <zeyu203@qq.com>
2018-08-29 11:11:12 +0800

    博客改造，草稿不分后缀

--- tools/diary_loader.php
+++ tools/diary_loader.php
@@ -5,12 +5,25 @@ LogOpt::init('diary_loader', true);
 $options = getopt('t:');
 
 $infos = array();
-$draft_file = DRAFT_PATH.'/draft.tpl';
-if (!file_exists($draft_file))
-{
+$draft_array = scandir(DRAFT_PATH);
+$draft_result = array();
+foreach ($draft_array as $draft_name) {
+	$match_count = preg_match('/^draft(\..*|$)/', $draft_name, $result);
+	if ($match_count > 0) {
+		$draft_result[] = $draft_name;
+	}
+}
+
+if (count($draft_result) > 1) {
+	echo 'Error：不止一个可选的 draft 文件'.PHP_EOL;
+	return;
+}
+if (empty($draft_result)) {
 	echo '指定日志的草稿不存在'.PHP_EOL;
 	return;
 }
+
+$draft_file = DRAFT_PATH.'/'.$draft_result[0];
 $infos['draft'] = file_get_contents($draft_file);
 $contents = TechlogTools::pre_treat_article ($infos['draft']);
 $image_ids = array();
