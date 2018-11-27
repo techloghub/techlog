f0854d6702635f4677471c12031447f599f87a8c
zeyu203 <zeyu203@qq.com>
2018-11-27 17:53:15 +0800

    增加注释

--- tools/turn_to_markdown.php
+++ tools/turn_to_markdown.php
@@ -4,7 +4,7 @@ require_once (__DIR__.'/../library/MarkdownTools.php');
 
 $options = getopt('i:');
 if (!isset ($options['i'])) {
-	echo 'usage: php turn_to_markdown.php -i article_id'.PHP_EOL;
+	echo 'usage: php turn_to_markdown.php -i [article_id|draft]'.PHP_EOL;
 	return;
 }
 $draft = null;
