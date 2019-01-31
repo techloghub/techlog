c4aeb4197c985a7814eab959b5802a925723d172
zeyu203 <zeyu203@qq.com>
2019-01-31 10:39:40 +0800

    增加测试页面

---
+++ controller/TempController.php
@@ -0,0 +1,18 @@
+<?php
+class TempController extends Controller
+{
+	public function hkexnewshkAction($query_params)
+	{
+		$this->display(__METHOD__, $params);
+	}
+
+	public function hkexnewsszAction($query_params)
+	{
+		$this->display(__METHOD__, $params);
+	}
+
+	public function hkexnewsshAction($query_params)
+	{
+		$this->display(__METHOD__, $params);
+	}
+}
