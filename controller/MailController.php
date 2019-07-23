e442b1356cf1d3e1ec24c32769e46410979d13d2
liuzeyu <liuzeyu@dibgroup.cn>
2019-07-23 15:38:08 +0800

    增加邮件发送接口异常捕获

--- controller/MailController.php
+++ controller/MailController.php
@@ -4,37 +4,45 @@ use PHPMailer\PHPMailer\PHPMailer;
 
 class MailController
 {
+    /**
+     * @param array $query_params
+     * @return array
+     */
     public function listAction($query_params) {
-        $subject = $_REQUEST['subject'];
-        $to = $_REQUEST['to'];
-        $content = $_REQUEST['content'];
-        $ishtml = $_REQUEST['html'];
+        try {
+            $subject = $_REQUEST['subject'];
+            $to = $_REQUEST['to'];
+            $content = $_REQUEST['content'];
+            $ishtml = $_REQUEST['html'];
 
-        $config = file_get_contents(CONF_PATH.'/config.json');
-        $config = json_decode($config, true);
-        $config = $config['mail'];
+            $config = file_get_contents(CONF_PATH . '/config.json');
+            $config = json_decode($config, true);
+            $config = $config['mail'];
 
-        $mail = new PHPMailer(true);
-        $mail->isSMTP();
-        $mail->Host       = $config['host'];
-        $mail->SMTPAuth   = true;
-        $mail->Username   = $config['username'];
-        $mail->Password   = $config['password'];
-        $mail->SMTPSecure = $config['smtpsecure'];
-        $mail->Port       = $config['port'];
+            $mail = new PHPMailer(true);
+            $mail->isSMTP();
+            $mail->Host = $config['host'];
+            $mail->SMTPAuth = true;
+            $mail->Username = $config['username'];
+            $mail->Password = $config['password'];
+            $mail->SMTPSecure = $config['smtpsecure'];
+            $mail->Port = $config['port'];
 
-        $mail->setFrom($config['username'], $config['nickname']);
-        $tolist = explode(',', $to);
-        foreach ($tolist as $to) {
-            $mail->addAddress($to);
+            $mail->setFrom($config['username'], $config['nickname']);
+            $tolist = explode(',', $to);
+            foreach ($tolist as $to) {
+                $mail->addAddress($to);
+            }
+            $mail->isHTML(intval($ishtml) == 1);
+            $mail->Subject = $subject;
+            $mail->Body = $content;
+            if (isset($_REQUEST['altcontent'])) {
+                $mail->AltBody = $_REQUEST['altcontent'];
+            }
+            $mail->send();
+            return array('code' => 0, 'msg' => '发送成功');
+        } catch (Exception $e) {
+            return array('code' => -1, 'msg' => '发送失败 - '.var_export($e, true));
         }
-        $mail->isHTML(intval($ishtml) == 1);
-        $mail->Subject = $subject;
-        $mail->Body    = $content;
-        if (isset($_REQUEST['altcontent'])) {
-            $mail->AltBody = $_REQUEST['altcontent'];
-        }
-        $mail->send();
-        return array('code' => 0, 'msg' => '发送成功');
     }
 }
