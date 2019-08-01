ddec0570b057b3a817ccc65cad80550677ebc4e8
zeyu203 <zeyu203@qq.com>
2019-08-01 13:17:06 +0800

    去除注释

--- controller/IndexController.php
+++ controller/IndexController.php
@@ -6,6 +6,13 @@ class IndexController extends Controller
 		$this->display(__METHOD__, $params);
 	}
 
+	public function tokenAction($params)
+	{
+        var_dump($params);
+        var_dump($_REQUEST);
+        exit;
+	}
+
 	public function notfoundAction($params)
 	{
 		header('HTTP/1.1 404 Not Found');
