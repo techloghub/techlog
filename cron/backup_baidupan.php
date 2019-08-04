99d268d0824298ce0e93ce807fd9af1b6dfbb744
liuzeyu <liuzeyu@dibgroup.cn>
2019-08-04 18:30:58 +0800

    测试百度云盘预上传接口

--- cron/backup_baidupan.php
+++ cron/backup_baidupan.php
@@ -1,4 +1,5 @@
 <?php
 require_once(__DIR__.'/../app/register.php');
+trigger_error(E_USER_NOTICE);
 $baidupanGateway = new BaiduPanGateway();
 $baidupanGateway->precreate('/home/zeyu/a.txt', '/techlog/test/a.txt');
