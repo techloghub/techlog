fc74c6a06706f063f309e97ed23974fe3a113cd2
zeyu203 <zeyu203@qq.com>
2020-04-08 10:35:26 +0800

    封面换为简体字

--- views/index/list.php
+++ views/index/list.php
@@ -15,11 +15,11 @@
 	<br />
 	<h1><p class="indexfont">龍潭齋</p></h1>
 	<br />
-	<p class="indexfont" style='font-family:"PT Serif","Georgia","Helvetica Neue",Arial,sans-serif' class="lead">這個世上只有兩樣東西永遠都不會衰老，一個是青春，一個是對知識的渴求</p>
-	<p style='font-family:"PT Serif","Georgia","Helvetica Neue",Arial,sans-serif;' class="indexfont"> ---- 熱愛生活，熱愛編碼，為了夢想奔跑的程序員</p>
+	<p class="indexfont" style='font-family:"PT Serif","Georgia","Helvetica Neue",Arial,sans-serif' class="lead">这个世上只有两样东西永远都不会衰老，一个是青春，一个是对知识的渴求</p>
+	<p style='font-family:"PT Serif","Georgia","Helvetica Neue",Arial,sans-serif;' class="indexfont"> ---- 热爱生活，热爱编码，为了梦想奔跑的程序员</p>
 	<br />
 	<p>
-		<a class="btn btn-lg btn-primary" href="/article/list/10182614" role="button" style='font-family:"Hiragino Sans GB", "Microsoft YaHei","WenQuanYi Micro Hei"'>博客簡介</a>
+		<a class="btn btn-lg btn-primary" href="/article/list/10182614" role="button" style='font-family:"Hiragino Sans GB", "Microsoft YaHei","WenQuanYi Micro Hei"'>博客简介</a>
 		<?php if ($params['is_root']): ?>
 		<a class="btn btn-lg btn-primary" href="http://admin.techlog.cn/techlog_manager/tasklist/list" role="button" style='font-family:"Hiragino Sans GB", "Microsoft YaHei","WenQuanYi Micro Hei"' target="_blank">任务管理</a>
 		<?php endif ?>
